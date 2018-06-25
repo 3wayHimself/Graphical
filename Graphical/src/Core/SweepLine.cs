568487d9a6370c03c85a8658b3944eb5621a83c5
Alvaro Ortega Pickmans <aortegapickmans@gmail.com>
2018-06-25 18:31:01 +0100

    Adding SweepLine Class

---
+++ Graphical/src/Core/SweepLine.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Graphical.Geometry;
+using Graphical.DataStructures;
+
+namespace Graphical.Core
+{
+    /// <summary>
+    /// Helper class to implement Bentley-Ottmann Algorithm for
+    /// polygon self-intersections and boolean operations.
+    /// </summary>
+    public static class SweepLine
+    {
+
+
+        
+    }
+
+    internal class SweepVertex : IComparable<SweepVertex>
+    {
+        public gVertex Vertex;
+        public SweepVertex Pair;
+        public bool IsStart;
+
+        public int CompareTo(SweepVertex other)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
